4f8b2530d66ebd87a3fc87ef782c78d98ae0e966
jaderUY <jaderdavidaraujoarboleda@gmail.com>
2026-05-23 00:04:54 -0500

    Cambio de rutas relativas

--- src/server/modules/products/product.pages.js
+++ src/server/modules/products/product.pages.js
@@ -6,13 +6,13 @@ const productService = require('./product.service');
 router.get('/products', async (req, res) => {
   const products = await productService.getProducts();
   const categories = await productService.getCategories();
-  res.render('products/list', { products, categories, title: 'Catálogo' });
+  res.render('modules/products/views/list', { products, categories, title: 'Catálogo' });
 });
 
 router.get('/products/:id', async (req, res) => {
   const product = await productService.getProductById(req.params.id);
   if (!product) return res.status(404).send('Producto no encontrado');
-  res.render('products/detail', { product, title: product.nombre });
+  res.render('modules/products/views/detail', { product, title: product.nombre });
 });
 
 module.exports = router;
